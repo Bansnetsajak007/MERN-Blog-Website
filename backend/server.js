2a2fc48fcdf81c0520d1ecdf83f7e3c06434b1c2
Dipsan Kadariya <dipsankadariya99@gmail.com>
2024-10-14 22:15:44 +0545

    CORS changes

--- backend/server.js
+++ backend/server.js
@@ -5,51 +5,51 @@ const Blogpost = require('./models/blogModel');
 const cors = require('cors');
 const app = express();
 
+// CORS configuration
+const corsOptions = {
+    origin: 'https://mern-blog-website-iota.vercel.app',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    credentials: true,
+};
 
-app.use(cors());
+app.use(cors(corsOptions));
 app.use(express.json());
 
 const PORT = process.env.PORT || 5000;
 
-
-
-//creating a new post
+// Creating a new post
 app.post('/create', async (req, res) => {
     try {
         const post = await Blogpost.create(req.body);
         res.status(200).json(post);
-    }
-    catch (error) {
-        console.log(error.message)
+    } catch (error) {
+        console.log(error.message);
         res.status(500).json({ message: error.message });
     }
-})
+});
 
-//fetching all the post
+// Fetching all the posts
 app.get('/', async (req, res) => {
     try {
         const posts = await Blogpost.find({});
         res.status(200).json(posts);
-    }
-    catch (error) {
+    } catch (error) {
         res.status(500).json({ message: error.message });
     }
-})
+});
 
-
-//fetching a single post
+// Fetching a single post
 app.get('/:id', async (req, res) => {
     try {
         const { id } = req.params;
         const post = await Blogpost.findById(id);
         res.status(200).json(post);
-    }
-    catch (error) {
+    } catch (error) {
         res.status(500).json({ message: error.message });
     }
-})
+});
 
-//updating a post 
+// Updating a post
 app.put('/:id', async (req, res) => {
     try {
         const { id } = req.params;
@@ -61,42 +61,33 @@ app.put('/:id', async (req, res) => {
 
         const updatedPost = await Blogpost.findById(id);
         res.status(200).json(updatedPost);
-    }
-
-    catch (error) {
+    } catch (error) {
         res.status(500).json({ message: error.message });
     }
+});
 
-})
+// Deleting a post
+app.delete('/:id', async (req, res) => {
+    try {
+        const { id } = req.params;
+        const post = await Blogpost.findByIdAndDelete(id);
 
-
-//deleting a post
-
-app.delete('/:id',async(req,res)=>{
-    try{
-        const {id} = req.params;
-        const post =  await  Blogpost.findByIdAndDelete(id);
-
-        if(!post){
-            res.status(404).json({message:error.message});
+        if (!post) {
+            return res.status(404).json({ message: `Cannot find post` });
         }
-        res.status(200).json(post)
-       
+
+        res.status(200).json(post);
+    } catch (error) {
+        res.status(500).json({ message: error.message });
     }
-    catch(error){
-     res.status(500).json({message:error.message});
-    }
-})
+});
 
-
-
-
-
+// Connect to MongoDB and start the server
 mongoose.connect(process.env.MONGODB_URI).then(() => {
     app.listen(PORT, () => {
         console.log(`Server started at port ${PORT}`);
-    })
+    });
     console.log('Connected to the database');
 }).catch((error) => {
     console.log(error);
-})
+});
